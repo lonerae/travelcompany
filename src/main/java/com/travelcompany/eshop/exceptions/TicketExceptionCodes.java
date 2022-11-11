1894cb6ba6fb88eb928f8b349d00f6a8e1c1c74a
Minas Alvertis <78692050+lonerae@users.noreply.github.com>
2022-11-11 10:47:56 +0200

    Update TicketExceptionCodes.java

--- src/main/java/com/travelcompany/eshop/exceptions/TicketExceptionCodes.java
+++ src/main/java/com/travelcompany/eshop/exceptions/TicketExceptionCodes.java
@@ -5,7 +5,7 @@ public class TicketExceptionCodes {
     public static final String NULL = "The ticket is null";
     public static final String TICKET_ITINERARY_IS_INVALID = "The itinerary does not exist";
     public static final String TICKET_CUSTOMER_IS_INVALID = "The customer does not exist";
-    public static final String FILE_NOT_ACCESSIBLE = "Cannot create ticket file";
+    public static final String FILE_NOT_ACCESSIBLE = "Ticket file is not accessible";
     public static final String INVALID_DATA = "Data cannot be parsed";
 
 }
