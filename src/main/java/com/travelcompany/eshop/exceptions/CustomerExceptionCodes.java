3df233c6a56fbfbc2ffe1c97ae5601b66b69e589
Minas Alvertis <78692050+lonerae@users.noreply.github.com>
2022-11-11 10:47:27 +0200

    Update CustomerExceptionCodes.java

--- src/main/java/com/travelcompany/eshop/exceptions/CustomerExceptionCodes.java
+++ src/main/java/com/travelcompany/eshop/exceptions/CustomerExceptionCodes.java
@@ -4,7 +4,7 @@ public class CustomerExceptionCodes {
 
     public static final String NULL = "The customer is null";
     public static final String CUSTOMER_INVALID_EMAIL = "Invalid customer email";
-    public static final String FILE_NOT_ACCESSIBLE = "Cannot create customer file";
+    public static final String FILE_NOT_ACCESSIBLE = "Customer file is not accessible";
     public static final String INVALID_DATA = "Data cannot be parsed";
 
 }
